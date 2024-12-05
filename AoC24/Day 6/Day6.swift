b9e5123299aeb32d58ef6dcfa59e8d9d77fef8a5
Mike Lewis <revmikelewis@gmail.com>
2024-12-05 15:32:53 -0600

    Day 5 complete

---
+++ AoC24/Day 6/Day6.swift
@@ -0,0 +1,16 @@
+//
+//  DayTemplate.swift
+//  AoC24
+//
+//  Created by Mike Lewis on 12/3/24.
+//
+
+import Foundation
+
+func DayXXXX(file: String, part: Int) -> String {
+    let lines = loadStringsFromFile(file)
+    
+    var tot = 0
+    
+    return "\(tot)"
+}
