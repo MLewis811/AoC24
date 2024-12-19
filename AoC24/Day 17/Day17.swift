ce717f46288b3cd876056e86188cfc8d5a968134
Mike Lewis <revmikelewis@gmail.com>
2024-12-19 16:12:52 -0600

    Day 17 setup

---
+++ AoC24/Day 17/Day17.swift
@@ -0,0 +1,17 @@
+//
+//  Day17.swift
+//  AoC24
+//
+//  Created by Mike Lewis on 12/18/24.
+//
+
+import Foundation
+
+func DayXXXX(file: String, part: Int) -> String {
+    var tot = 0
+    let lines = loadStringsFromFile(file)
+    
+
+    print(tot)
+    return "\(tot)"
+}
